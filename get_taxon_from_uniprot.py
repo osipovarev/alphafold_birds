9dc786be5a0bff9ef60e3bb77d789874446d86f1
Ekaterina Osipova <osipovarev@gmail.com>
2023-02-11 11:58:29 -0500

    fixed minor bugs

--- get_taxon_from_uniprot.py
+++ get_taxon_from_uniprot.py
@@ -30,7 +30,7 @@ def main():
     args = parser.parse_args()
 
     ## Parse arguments
-    taxis = args.taxids
+    taxids = args.taxids
     outfile = args.outfile
 
     ## Construct the query url and request data from uniprot
